fa6144c71a29d5f0db7896dc3d638f8e4fd000cf
Robert Horvath <robert.horvath@catalysts.cc>
2015-09-16 16:16:00 +0200

    Initial commit: Firefox31

---
+++ firefox31/entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ARTIFACTS=/tmp/artifacts
+DISPLAY=:1
+SCREEN=0
+RESOLUTION=1210x810
+SCREENCASTNR=0
+
+mkdir -p $ARTIFACTS
+
+function wrap() {
+  $@ > $ARTIFACTS/$1.out 2>> $ARTIFACTS/$1.err &
+}
+
+function intHandler() {
+  pkill -2 avconv
+}
+
+function startNewScreencast() {
+  wrap avconv \
+  -f x11grab \
+  -s $RESOLUTION \
+  -i $DISPLAY.$SCREEN+0,0 \
+  -r 20 \
+  -vcodec libx264 \
+  $ARTIFACTS/screencast_$SCREENCASTNR.mp4
+
+  ((SCREENCASTNR++))
+  wait
+}
+
+trap intHandler TERM INT
+trap startNewScreencast SIGUSR1
+
+wrap Xvfb \
+  $DISPLAY \
+  -screen $SCREEN \
+  ${RESOLUTION}x24
+
+sleep 1
+
+wrap x11vnc \
+  -geometry $RESOLUTION \
+  -display $DISPLAY \
+  -shared \
+  -forever \
+  -passwd stsylatac
+
+wrap tcpdump \
+  -w $ARTIFACTS/tcpdump.pcap
+
+DISPLAY=$DISPLAY wrap java -jar selenium-server-standalone.jar
+
+wait
